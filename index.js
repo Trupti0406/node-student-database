6d920c0d075a1766e237c50b7dafc6ca6b11f1d8
trupti0406 <truptivijayyadav27@gmail.com>
2023-02-04 01:21:22 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const storage = require("node-persist");
 const app = express();
-// const jsonParser = require("body-parser");
 app.use(express.json());
 
 const init = async () => {
@@ -10,6 +9,7 @@ const init = async () => {
 
 init();
 
+// Creating a post request that accepts student details
 app.post("/student", async (req, res) => {
   const student = {
     id: req.body.id,
@@ -19,19 +19,15 @@ app.post("/student", async (req, res) => {
 
   const allStudents = (await storage.getItem("students")) || [];
   allStudents.push(student);
-  //   console.log(student);
 
   await storage.setItem("students", allStudents);
-
   res.status(200).json({ success: true });
 });
 
-// Sending HTML back with GET request
+// Sending HTML back with GET request (All Students)
 app.get("/allStudents", async (req, res) => {
   const allStudents = (await storage.getItem("students")) || [];
-
-  let htmlCode = "<h1>All Students Data!</h1><br>";
-
+  let htmlCode = "<h1>All Students Data!</h1>";
   allStudents.forEach((student) => {
     htmlCode += `
     <div>
@@ -41,6 +37,51 @@ app.get("/allStudents", async (req, res) => {
     </div> <hr>
     `;
   });
+  res.send(htmlCode);
+});
+
+// GET request for retrieving data of a particular student using ID
+app.get("/student/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const allStudents = await storage.getItem("students");
+  const student = allStudents.filter((student) => id === student.id);
+  //   console.log(student);
+
+  let htmlCode = `<h1>The ID belongs to:</h1>`;
+  student.forEach((targetIdStudent) => {
+    htmlCode += `<div>
+        <h3> Student id: ${targetIdStudent.id} </h3>
+        <h3> Student Name: ${targetIdStudent.name} </h3>
+        <h3> Student GPA: ${targetIdStudent.gpa} </h3>
+    </div> <hr> `;
+  });
+  res.send(htmlCode);
+});
+
+// GET request for retrieving data of a student with maximun GPA
+app.get("/topper", async (req, res) => {
+  const allStudents = await storage.getItem("students");
+  const gpaArray = allStudents.map((a) => a.gpa); //Storing all GPAs in seperate array
+  let highest = Math.max(...gpaArray);
+  console.log("Highets GPA is: " + highest);
+
+  const topper = allStudents.filter((student) => {
+    if (student.gpa === highest) {
+      return student;
+    }
+  });
+
+  let htmlCode = `<h1> Topper student is: </h1>`;
+  topper.forEach((topperStudent) => {
+    htmlCode += `
+        <div>
+        <h3> Student id: ${topperStudent.id} </h3>
+        <h3> Student Name: ${topperStudent.name} </h3>
+        <h3> Student GPA: ${topperStudent.gpa} </h3>
+    </div> <hr>
+        `;
+  });
+
   res.send(htmlCode);
 });
 
